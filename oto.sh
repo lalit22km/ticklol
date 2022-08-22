c86537d6e788db6c28bd98d47da0ddd9e37423bf
Lalit Kumar <61497570+lalit22km@users.noreply.github.com>
2022-08-22 21:25:07 +0530

    Create oto.sh

---
+++ oto.sh
@@ -0,0 +1,6 @@
+wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz > /dev/null
+mkdir OtohitsApp
+cd OtohitsApp
+echo "/login:2927d9ef-2e83-495d-96fd-2613dc5f0dc6" > otohits.ini
+echo "/autoupdate" >> otohits.ini
+tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
