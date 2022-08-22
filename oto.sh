836d475cd97bceed87d5d09b51ad82e1f69badd0
Lalit Kumar <61497570+lalit22km@users.noreply.github.com>
2022-08-22 21:36:00 +0530

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -4,3 +4,6 @@ cd OtohitsApp
 echo "/login:2927d9ef-2e83-495d-96fd-2613dc5f0dc6" > otohits.ini
 echo "/autoupdate" >> otohits.ini
 tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
+sudo apt-get install -y libgbm-dev
+sudo apt-get install libasound2
+sudo apt install libgtk2.0-0
