475eb362c413ab4e65f9e41b5242c04b32d24082
Lalit Kumar <61497570+lalit22km@users.noreply.github.com>
2022-08-26 01:02:14 +0530

    Update peer2profit.sh

--- peer2profit.sh
+++ peer2profit.sh
@@ -1,6 +1,7 @@
 sudo apt update -y
 sudo apt install jwm -y
 sudo apt install xrdp -y
+sudo apt --fix-broken install -y
 sudo apt-get -y install libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev
 wget https://updates.peer2profit.app/peer2profit_0.48_amd64.deb
 sudo dpkg -i peer2profit_0.48_amd64.deb
